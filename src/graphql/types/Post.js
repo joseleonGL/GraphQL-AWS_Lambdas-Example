912f88207a1ba68a44456fbaf096a32c34650137
Mario Leon <jose.leon@gorillalogic.com>
2019-06-07 16:55:16 -0600

    Adding Reddit POST GraphQL schema, severless express wrapper

---
+++ src/graphql/types/Post.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const GraphQL = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLID,
+    GraphQLInt,
+} = GraphQL;
+
+const PostType = new GraphQL.GraphQLObjectType({
+    name: 'Post',
+    description: 'Post Type, For all the posts present in Reddit',
+    fields: () => ({
+        id: {
+            type: GraphQLID,
+            description: 'ID of the post'
+        },
+        title: {
+            type: GraphQLString,
+            description: 'Title of the post'
+        },
+        url: {
+            type: GraphQLString,
+            description: 'URL of the post'
+        },
+        author: {
+            type: GraphQLString,
+            description: 'Name of the Author who created the post'
+        },
+        ups: {
+            type: GraphQLInt,
+            description: 'Total number of Upvotes received for this post'
+        },
+        downs: {
+            type: GraphQLInt,
+            description: 'Total number of Downvotes received for this post'
+        },
+        content: {
+            type: GraphQLString,
+            description: 'Markdown content of the post'
+        }
+    })
+});
+
+module.exports = PostType;
